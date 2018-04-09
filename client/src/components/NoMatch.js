eb57dc1ccc844e3de8c1053686dbdd3fdea7b8b0
Melissa Hill <mhill2040@gmail.com>
2018-04-08 20:01:07 -0600

    workin on no match

--- client/src/components/NoMatch.js
+++ client/src/components/NoMatch.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { Header, Segment, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
-import dpsLogo from '../images/dpsLogo.svg';
+import Beer1 from '../images/beer1.jpg';
 
 class NoMatch extends Component {
   render() {
     return(
       <Segment basic style={styles.fullHeight}>
         <Segment basic textAlign='center'>
-          <Image style={styles.centered} size='tiny' src={dpsLogo} alt='DevPoint Studios Logo' />
-          <Header as='h1' style={styles.header}>DevPoint Studios React Assignment</Header>
+          <Image style={styles.centered} size='large' src={Beer1} alt='beer1' />
+          <Header as='h1' style={styles.header}>Uh-oh, you already drank your beer!</Header>
         </Segment>
         <Header as='h1' textAlign='center' inverted>
           Page Not Found
@@ -31,5 +31,40 @@ const styles = {
     height: '100vh',
   },
 }
-
-export default NoMatch;
+//
+// export default NoMatch;
+// import React, { Component } from 'react';
+// import { Header, Segment, Image } from 'semantic-ui-react';
+// import { Link } from 'react-router-dom';
+// import dpsLogo from '../images/dpsLogo.svg';
+//
+// class NoMatch extends Component {
+//   render() {
+//     return(
+//       <Segment basic style={styles.fullHeight}>
+//         <Segment basic textAlign='center'>
+//           <Image style={styles.centered} size='tiny' src={dpsLogo} alt='DevPoint Studios Logo' />
+//           <Header as='h1' style={styles.header}>DevPoint Studios React Assignment</Header>
+//         </Segment>
+//         <Header as='h1' textAlign='center' inverted>
+//           Page Not Found
+//           <Link to='/'> - Go Home</Link>
+//         </Header>
+//       </Segment>
+//     );
+//   }
+// }
+//
+// const styles = {
+//   centered: {
+//     margin: '0 auto',
+//   },
+//   header: {
+//     color: '#2ecc40'
+//   },
+//   fullHeight: {
+//     height: '100vh',
+//   },
+// }
+//
+// export default NoMatch;
